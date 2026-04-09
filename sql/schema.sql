27633d0477eb29c64d1956f952b26ff022062c1b
Harnoor Singh <singhharnoor@gmail.com>
2026-04-09 11:29:14 +0200

    change comments

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS social_media_db;
 USE social_media_db;
 
--- users table
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -30,7 +30,7 @@ CREATE TABLE comments (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- unique_like stops the same user liking a post more than once
+#unique_like stops the same user liking a post more than once
 CREATE TABLE likes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE follows (
     FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- messages between two users
+
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     sender_id INT NOT NULL,
@@ -61,18 +61,16 @@ CREATE TABLE messages (
     FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- indexes to make searches faster
 
--- when we look up posts by a user
+
+# when we look up posts by a user
 CREATE INDEX idx_posts_user ON posts(user_id);
--- comments are usually searched by post
+# comments are usually searched by post
 CREATE INDEX idx_comments_post ON comments(post_id);
 
--- also need to find comments made by a user
+# also need to find comments made by a user
 CREATE INDEX idx_comments_user ON comments(user_id);
 
--- to count likes on a post quickly
 CREATE INDEX idx_likes_post ON likes(post_id);
 
--- to get messages sent to a specific user
 CREATE INDEX idx_messages_receiver ON messages(receiver_id);
